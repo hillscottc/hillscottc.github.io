c95a192d4cfcb335589cb6157820228eaf5429e4
Scott Hill <scott289@gmail.com>
2014-06-19 21:21:52 -0700

    tm solve

--- algos/floyd.py
+++ algos/floyd.py
@@ -5,65 +5,61 @@ My implementation and utils for the Floyd Warshall Algorithm.
 pair of vertices in a weighted graph.)
 """
 
-
-def adj(g):
-    vertices = g.keys()
-    return {v1: {v2: 0 if v1 == v2 else g[v1].get(v2, float('inf'))
-                 for v2 in vertices}
-            for v1 in vertices}
-
-
-def paths_to_list(paths):
-    return {v1: {v2:  0 if v1 == v2 else g[v1].get(v2, float('inf'))
+def chars_to_adj_matrix(*paths):
+    return {v1: {v2: float('inf') if v1 != v2 and paths[v1][v2] == '0' else int(paths[v1][v2])
                  for v2 in range(len(paths))}
             for v1 in range(len(paths))}
 
 
-# def floyd(graph):
-#     """The Floyd-Warshall...shortest paths for all vertices."""
-#     vertices = graph.keys()
-#     fg = graph
-#     for v2 in vertices:
-#         fg = {v1: {v3: min(fg[v1][v3], fg[v1][v2] + fg[v2][v3])
-#                   for v3 in vertices}
+def floyd(graph):
+    vertices = graph.keys()
+    d = graph
+    for v2 in vertices:
+        d = {v1: {v3: min(d[v1][v3], d[v1][v2] + d[v2][v3])
+                  for v3 in vertices}
+             for v1 in vertices}
+    return d
 
+
+def get_results(fg):
+    reach_all, by_all = 0, 0
+    for vert in fg.keys():
+        if not [v for v in fg[vert].values() if v == float('inf')]:
+            reach_all += 1
+
+    return reach_all, by_all
 
 
 if __name__ == '__main__':
     from pprint import pprint
 
     # From the probs given examples, as (paths, returns),
-    # where returns is (count-can-reach-all, count-reachable-by-all)
+    # where 'results' = (count-can-reach-all, count-reachable-by-all)
     test = (
         {'paths': ("010", "000", "110"), 'results': (1, 1)},
         {'paths': ("0010", "1000", "1100", "1000"), 'results': (1, 3)},
-        # {'paths': ("01000", "00100","00010", "00001", "10000"), 'results': (5, 5)},
-        # {'paths': ("0110000", "1000100", "0000001", "0010000", "0110000",
-        #   "1000010", "0001000"), 'results': (1, 3)}
+        {'paths': ("01000", "00100","00010", "00001", "10000"), 'results': (5, 5)},
+        {'paths': ("0110000", "1000100", "0000001", "0010000", "0110000",
+         "1000010", "0001000"), 'results': (1, 3)}
     )
 
+    for test in test:
+        print "\nPaths:"
+        pprint(test['paths'])
 
-    matrix = paths_to_list(test[0]['paths'])
+        print "\nAdj Matrix:"
+        matrix = chars_to_adj_matrix(*test['paths'])
+        pprint(matrix)
 
-    pprint(matrix)
+        print "\nFloyd"
+        fg = floyd(matrix)
+        pprint(fg)
 
-    # for test in test_data:
-    #     print "\nPaths:"
-    #     pprint(test['paths'])
-    #     graph = make_adj(*test['paths'])
-    #     print "\nGraph:"
-    #     pprint(graph)
+        print "\nResults"
+        r = get_results(fg)
 
+        print("Can reach all", r[0])
+        assert(r[0] == test['results'][0])
 
-    # pprint(adj(*paths))
-    # print floyd(make_adj(*paths))
-
-    # fg = floyd(graph)
-    # print "\nFloyd:"
-    # pprint(fg)
-
-    # reach_all, by_all = 0, 0
-    # for key, nodes in fg.iteritems():
-    #     print "sum of %s is %d" % (key, sum([1 for v in nodes.values() if v == 0]))
-
-
+        # print("Reachable by all", r[1])
+        # assert(r[1] == test['results'][1])
---
+++ problems/teambuilder_solved.md
@@ -0,0 +1,77 @@
+---
+layout: default
+title: teambuilder_solved.md
+---
+
+Solution to [Teambuilder Problem](teambuilder.html)
+
+    """
+    Use Floyd Warshall to solve Teambuilder
+    """
+    from pprint import pprint
+
+
+    def chars_to_adj_matrix(*paths):
+        return {v1: {v2: float('inf') if v1 != v2 and paths[v1][v2] == '0'
+                                      else int(paths[v1][v2])
+                     for v2 in range(len(paths))}
+                for v1 in range(len(paths))}
+
+
+    def floyd(adj_matrix):
+        vertices = adj_matrix.keys()
+        d = adj_matrix
+        for v2 in vertices:
+            d = {v1: {v3: min(d[v1][v3], d[v1][v2] + d[v2][v3])
+                      for v3 in vertices}
+                 for v1 in vertices}
+        return d
+
+
+    def get_results(floyd_graph):
+        reach_all, by_all = 0, 0
+        for vert in floyd_graph.keys():
+            if not [v for v in floyd_graph[vert].values() if v == float('inf')]:
+                reach_all += 1
+
+        return reach_all, by_all
+
+
+    if __name__ == '__main__':
+        """
+        Test the solutions from the probs given examples.
+        'results' = (count-can-reach-all, count-reachable-by-all)
+        """
+
+        test = (
+            {'paths': ("010", "000", "110"),
+             'results': (1, 1)},
+            {'paths': ("0010", "1000", "1100", "1000"),
+             'results': (1, 3)},
+            {'paths': ("01000", "00100", "00010", "00001", "10000"),
+             'results': (5, 5)},
+            {'paths': ("0110000", "1000100", "0000001",
+                       "0010000", "0110000", "1000010", "0001000"),
+             'results': (1, 3)}
+        )
+
+        for test in test:
+            print "\nPaths:"
+            pprint(test['paths'])
+
+            print "\nAdj Matrix:"
+            matrix = chars_to_adj_matrix(*test['paths'])
+            pprint(matrix)
+
+            print "\nFloyd"
+            floyd_graph = floyd(matrix)
+            pprint(floyd_graph)
+
+            print "\nResults"
+            r = get_results(floyd_graph)
+
+            print("Can reach all", r[0])
+            assert(r[0] == test['results'][0])
+
+            # print("Reachable by all", r[1])
+            # assert(r[1] == test['results'][1])
